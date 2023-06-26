283b94ebff465e12e715080c4e94c5c8ba4e3c4d
Nelson fai <101453696+nelsonfai@users.noreply.github.com>
2023-06-26 23:54:59 +0200

    Update settings.py

--- solarflash/settings.py
+++ solarflash/settings.py
@@ -131,6 +131,14 @@ STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 STATICFILES_DIRS = (
     os.path.join(BASE_DIR, 'static'),
 )
+
+MEDIA_URL = "/media/"
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+
+# Default primary key field type
+# https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
+
+DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
 
